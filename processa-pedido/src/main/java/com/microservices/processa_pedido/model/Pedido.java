0fe8c7ded70a2a1164e5eee92dcc1ac81f2b924b
joaov12 <jvfs12th@gmail.com>
2025-06-02 08:26:15 -0300

    build: Cria modelo de pedido

---
+++ processa-pedido/src/main/java/com/microservices/processa_pedido/model/Pedido.java
@@ -0,0 +1,76 @@
+package com.microservices.processa_pedido.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_pedidos")
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+    private Double valorTotal;
+    private String status;
+    private String  localEntrega;
+
+    public Pedido() {
+    }
+
+    public Pedido(String descricao, Double valorTotal, String status, String localEntrega) {
+        this.descricao = descricao;
+        this.valorTotal = valorTotal;
+        this.status = status;
+        this.localEntrega = localEntrega;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocalEntrega() {
+        return localEntrega;
+    }
+    public void setLocalEntrega(String localEntrega) {
+        this.localEntrega = localEntrega;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                ", valorTotal=" + valorTotal +
+                ", status='" + status + '\'' +
+                ", localEntrega='" + localEntrega + '\'' +
+                '}';
+    }
+}
