81f7b8248b0762dc5c192fbddc3376d76d21f11f
joaov12 <jvfs12th@gmail.com>
2025-06-02 08:25:50 -0300

    init: Inicia serviço de processar pedidos

---
+++ processa-pedido/src/main/java/com/microservices/processa_pedido/ProcessaPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.processa_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessaPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessaPedidoApplication.class, args);
+	}
+
+}
---
+++ processa-pedido/src/test/java/com/microservices/processa_pedido/ProcessaPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.processa_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessaPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
