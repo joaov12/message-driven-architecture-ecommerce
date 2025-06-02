c8e516f790fd17f2a6edff7389741b70db0c0151
joaov12 <jvfs12th@gmail.com>
2025-06-02 13:27:36 -0300

    init: Inicia microsserviço para processamento de entrega

---
+++ processa-entrega/src/main/java/com/microservices/processa_entrega/ProcessaEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.processa_entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessaEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessaEntregaApplication.class, args);
+	}
+
+}
---
+++ processa-entrega/src/test/java/com/microservices/processa_entrega/ProcessaEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.processa_entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessaEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
