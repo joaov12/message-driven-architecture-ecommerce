b55a343cfa7902d79cf99decb2e62be46100736d
joaov12 <jvfs12th@gmail.com>
2025-06-03 10:38:09 -0300

    init: Inicia microsserviço de notificações

---
+++ processa-notificacao/src/test/java/com/microsservice/processa_notificacao/ProcessaNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservice.processa_notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessaNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
